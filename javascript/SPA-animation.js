8c709aac7e2cf4f401edc518693b6d587be07ede
Jerry Chen <jerryq.chen@gmail.com>
2022-02-21 22:47:12 +0800

    fixed some bugs for the unclickable input due to z-index

--- javascript/SPA-animation.js
+++ javascript/SPA-animation.js
@@ -1,40 +1,27 @@
-//show app brief
+// //show app brief
 document.querySelector("#barIcon").addEventListener("click", function () {
   let appBrief = document.querySelector("#appBrief");
   appBrief.classList.remove("hideLeft");
   appBrief.classList.add("show");
 });
 
-//hide app brief
-document.querySelector("#appBrief").addEventListener("click", function () {
+// //hide app brief
+document.querySelector("#appBriefHide").addEventListener("click", function () {
   let appBrief = document.querySelector("#appBrief");
   appBrief.classList.remove("show");
   appBrief.classList.add("hideLeft");
 });
 
-//show search OTC drug page
+//show drug page
 document.querySelector("#searchOTCBtn").addEventListener("click", function () {
-  let appBrief = document.querySelector("#searchOTCPage");
-  appBrief.classList.remove("hideTop");
-  appBrief.classList.add("show");
+  let page = document.querySelector("#searchOTCPage");
+  page.classList.remove("hideLeft");
+  page.classList.add("show");
 });
 
-//hide search OTC drug page
-document.querySelector(".greyContainer").addEventListener("click", function () {
-  let appBrief = document.querySelector("#searchOTCPage");
-  appBrief.classList.remove("show");
-  appBrief.classList.add("hideTop");
-});
-
-//e-pharmacy page
+//show pharmacy page
 document.querySelector("#ePharmacyBtn").addEventListener("click", function () {
-  let appBrief = document.querySelector("#ePharmacyPage");
-  appBrief.classList.remove("hideTop");
-  appBrief.classList.add("show");
-});
-
-document.querySelector("#ePharmacyPage").addEventListener("click", function () {
-  let appBrief = document.querySelector("#ePharmacyPage");
-  appBrief.classList.remove("show");
-  appBrief.classList.add("hideTop");
+  let page = document.querySelector("#ePharmacyPage");
+  page.classList.remove("hideLeft");
+  page.classList.add("show");
 });
--- javascript/getUserLocation.js
+++ javascript/getUserLocation.js
@@ -1,3 +1,21 @@
 //https://developer.mozilla.org/en-US/docs/Web/API/Geolocation/getCurrentPosition
 
-function getUserLocation() {}
+function getUserLocation() {
+  function success(pos) {
+    var crd = pos.coords;
+
+    // return [crd.latitude, crd.longitude];
+
+    console.log("Your current position is:");
+    console.log(`Latitude : ${crd.latitude}`);
+    console.log(`Longitude: ${crd.longitude}`);
+
+    return "user location";
+  }
+
+  function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+  }
+
+  navigator.geolocation.getCurrentPosition(success, error);
+}
