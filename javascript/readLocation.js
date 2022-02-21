fbe87bbc35216340c7a5491374b9d634fe0cc4ef
Jerry Chen <jerryq.chen@gmail.com>
2022-02-21 14:24:34 +0800

    made search database for searching drug store

--- javascript/readLocation.js
+++ javascript/readLocation.js
@@ -2,6 +2,8 @@ async function readPharmacyLocationJson(map) {
   let response = await axios.get("../localData/pharmacyLocation.geojson");
   let pharmacistNames = await extractPharmacistData();
 
+  let pharmacySearchArrayData = [];
+
   let locationArray = response.data.features;
   let markerClusterGroup = L.markerClusterGroup();
 
@@ -27,6 +29,19 @@ async function readPharmacyLocationJson(map) {
     let postalCode = columns[0].innerHTML;
     let pharmacistName = "";
 
+    let oneAddress = [
+      `
+    ${pharmacyName},
+    ${buildingName},
+    blk ${blkNo},
+    ${roadName},
+    #${levelNo}-${unitNo},
+    Singapore ${postalCode},
+    `,
+    ];
+
+    pharmacySearchArrayData.push(oneAddress);
+
     // if there is pharmacist name data, add to the pop up;
     if (pharmacistNames[postalCode]) {
       pharmacistName = pharmacistNames[postalCode];
@@ -48,4 +63,43 @@ async function readPharmacyLocationJson(map) {
   }
   // return columns;
   markerClusterGroup.addTo(map);
+  return pharmacySearchArrayData;
+}
+
+async function extractAddressForSearch() {
+  let response = await axios.get("../localData/pharmacyLocation.geojson");
+
+  let pharmacySearchArrayData = [];
+
+  let locationArray = response.data.features;
+
+  // return locationArray;
+  for (let el of locationArray) {
+    let description = el.properties.Description;
+
+    //create marker description
+    let dummyDiv = document.createElement("div");
+    dummyDiv.innerHTML = description;
+    //extract all table data from geoJson data
+    let columns = dummyDiv.querySelectorAll("td");
+
+    let lat = Number(el.geometry.coordinates[1]);
+    let lng = Number(el.geometry.coordinates[0]);
+    let pharmacyName = columns[6].innerHTML;
+    let buildingName = columns[1].innerHTML;
+    let blkNo = columns[5].innerHTML;
+    let roadName = columns[4].innerHTML;
+    let levelNo = columns[3].innerHTML;
+    let unitNo = columns[2].innerHTML;
+    let postalCode = columns[0].innerHTML;
+
+    let oneAddress = [
+      `${pharmacyName}, ${buildingName} blk ${blkNo} ${roadName} #${levelNo}-${unitNo} Singapore ${postalCode},`.toLowerCase(),
+      lat,
+      lng,
+    ];
+
+    pharmacySearchArrayData.push(oneAddress);
+  }
+  return pharmacySearchArrayData;
 }
