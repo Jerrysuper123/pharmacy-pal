8c3439a51845e8427cfe4c5d6711a143e1338417
Jerry Chen <jerryq.chen@gmail.com>
2022-02-21 11:13:24 +0800

    extracted address from td from geoJson dataand displayed on the marker

--- javascript/main.js
+++ javascript/main.js
@@ -8,15 +8,42 @@ async function readPharmacyLocationJson() {
   let locationArray = response.data.features;
 
   // return locationArray;
-
+  let columns;
   for (let el of locationArray) {
     let lat = Number(el.geometry.coordinates[1]);
     let lng = Number(el.geometry.coordinates[0]);
     let description = el.properties.Description;
     let marker = L.marker([lat, lng]);
-    marker.bindPopup(description);
+
+    //create marker description
+    let dummyDiv = document.createElement("div");
+    dummyDiv.innerHTML = description;
+    //extract all table data from geoJson data
+    columns = dummyDiv.querySelectorAll("td");
+
+    let pharmacyName = columns[6].innerHTML;
+    let buildingName = columns[1].innerHTML;
+    let blkNo = columns[5].innerHTML;
+    let roadName = columns[4].innerHTML;
+    let levelNo = columns[3].innerHTML;
+    let unitNo = columns[2].innerHTML;
+    let postalCode = columns[0].innerHTML;
+
+    marker.bindPopup(`
+    <ul>
+      <li>${pharmacyName}</li>
+      <li>${buildingName}</li>
+      <li>Blk ${blkNo} ${roadName} #${levelNo}-${unitNo}</li>
+      <li>Singapore ${postalCode}</li>
+    </ul>
+    `);
+
+    //testing to see the full table
+    // marker.bindPopup(description);
+
     marker.addTo(markerClusterGroup);
   }
+  // return columns;
 }
 
 let singapore = [1.29, 103.85];
