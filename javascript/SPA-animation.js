0cd341fb2166aa7dbd91611ab28f6b22ed0e5038
Jerry Chen <jerryq.chen@gmail.com>
2022-02-22 06:59:43 +0800

    fixed SPA bugs and added some icons for navigation among different pages

--- javascript/SPA-animation.js
+++ javascript/SPA-animation.js
@@ -1,16 +1,25 @@
-// //show app brief
+//show app brief through the barIcon
 document.querySelector("#barIcon").addEventListener("click", function () {
-  let appBrief = document.querySelector("#appBrief");
-  appBrief.classList.remove("hideLeft");
-  appBrief.classList.add("show");
+  let page = document.querySelector("#appBrief");
+  page.classList.remove("hideLeft");
+  page.classList.add("show");
 });
 
-// //hide app brief
+// //hide app brief through grey area
 document.querySelector("#appBriefHide").addEventListener("click", function () {
-  let appBrief = document.querySelector("#appBrief");
-  appBrief.classList.remove("show");
-  appBrief.classList.add("hideLeft");
+  let page = document.querySelector("#appBrief");
+  page.classList.remove("show");
+  page.classList.add("hideLeft");
 });
+
+//hide app brief thru x button
+document
+  .querySelector("#closeAppBriefBtn")
+  .addEventListener("click", function () {
+    let page = document.querySelector("#appBrief");
+    page.classList.remove("show");
+    page.classList.add("hideLeft");
+  });
 
 //show drug page
 document.querySelector("#searchOTCBtn").addEventListener("click", function () {
@@ -19,9 +28,27 @@ document.querySelector("#searchOTCBtn").addEventListener("click", function () {
   page.classList.add("show");
 });
 
+//hide drug page through back button
+document
+  .querySelector("#otcPageBackBtn")
+  .addEventListener("click", function () {
+    let page = document.querySelector("#searchOTCPage");
+    page.classList.remove("show");
+    page.classList.add("hideLeft");
+  });
+
 //show pharmacy page
 document.querySelector("#ePharmacyBtn").addEventListener("click", function () {
   let page = document.querySelector("#ePharmacyPage");
   page.classList.remove("hideLeft");
   page.classList.add("show");
 });
+
+//hide epharmacy page through back button
+document
+  .querySelector("#ePharmacyBackBtn")
+  .addEventListener("click", function () {
+    let page = document.querySelector("#ePharmacyPage");
+    page.classList.remove("show");
+    page.classList.add("hideLeft");
+  });
