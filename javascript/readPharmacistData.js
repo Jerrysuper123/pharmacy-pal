83f7319e924481abb2c64b6e16a268facd327c8a
Jerry <jerry.chen@trentglobal.edu.sg>
2022-03-07 02:58:56 +0000

    refactored codes in readPharmacistData.js

--- javascript/readPharmacistData.js
+++ javascript/readPharmacistData.js
@@ -1,16 +1,19 @@
 async function readPharmacist() {
   let BASE_URL = "https://data.gov.sg/api/action/datastore_search";
-
   var data = {
     resource_id: "16db7800-d81e-4d0d-9d59-936f2c10d668", // the resource id
     limit: 300,
   };
 
-  let response = await axios.get(BASE_URL, {
+  let response;
+  try{
+    response = await axios.get(BASE_URL, {
     params: data,
   });
-  // console.log(response.data.result.records);
-  return response.data.result.records;
+    return response.data.result.records;
+  }catch(error){
+    console.log(error);
+  }
 }
 
 function transformPharmacistData(rawData) {
@@ -22,7 +25,6 @@ function transformPharmacistData(rawData) {
     let postalCode = extractPostCode(address);
     transformedData[postalCode] = el["pharmacist-in-charge"];
   }
-  //   console.log(rawData);
   return transformedData;
 }
 
@@ -35,6 +37,5 @@ function extractPostCode(address) {
 async function extractPharmacistData() {
   let rawData = await readPharmacist();
   let transformedData = transformPharmacistData(rawData);
-
   return transformedData;
 }
